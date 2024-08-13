8e923bb1f0fb3cdcd6c36c9db16aab98465be6c8
PuimegeT <thibault.puimege@student.kdg.be>
2024-08-13 10:42:47 +0200

    fix: unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
 import "./App.css"
 
 class LambdaDemo extends Component {
@@ -8,24 +7,12 @@ class LambdaDemo extends Component {
     this.state = { loading: false, msg: null }
   }
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
   render() {
-    const { loading, msg } = this.state
+    const { } = this.state
 
     return (
       <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
         <br />
-        <span>{msg}</span>
       </p>
     )
   }
