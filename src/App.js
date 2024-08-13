a50c44e86bd410f0bc2150d27a6d37dd714b51af
PuimegeT <thibault.puimege@student.kdg.be>
2024-08-13 10:43:53 +0200

    fix: unused component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
 import React, { Component } from "react"
 import "./App.css"
-
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  render() {
-    const { } = this.state
-
-    return (
-      <p>
-        <br />
-      </p>
-    )
-  }
-}
-
 class App extends Component {
   render() {
     return (
